0251d63e570bce603ad971cb1364321d014464fb
iamiratsh <167172029+iamiratsh@users.noreply.github.com>
2024-07-11 16:51:46 +0330

    Created using Colab

---
+++ تابع بنویسید که لیستی از اعداد را از ورودی دریافت کرده و ب خروجی تابع ارسال کند.py
@@ -0,0 +1,49 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyOSac7r77cDAjds+e0qTJmA",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/iamiratsh/amir-hossein-atashkar/blob/main/%D8%AA%D8%A7%D8%A8%D8%B9%20%D8%A8%D9%86%D9%88%DB%8C%D8%B3%DB%8C%D8%AF%20%DA%A9%D9%87%20%D9%84%DB%8C%D8%B3%D8%AA%DB%8C%20%D8%A7%D8%B2%20%D8%A7%D8%B9%D8%AF%D8%A7%D8%AF%20%D8%B1%D8%A7%20%D8%A7%D8%B2%20%D9%88%D8%B1%D9%88%D8%AF%DB%8C%20%D8%AF%D8%B1%DB%8C%D8%A7%D9%81%D8%AA%20%DA%A9%D8%B1%D8%AF%D9%87%20%D9%88%20%D8%A8%20%D8%AE%D8%B1%D9%88%D8%AC%DB%8C%20%D8%AA%D8%A7%D8%A8%D8%B9%20%D8%A7%D8%B1%D8%B3%D8%A7%D9%84%20%DA%A9%D9%86%D8%AF.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "#تابع بنویسید که لیستی از اعداد را از ورودی دریافت کرده و ب خروجی تابع ارسال کند\n",
+        "def inputlist():\n",
+        "    L={}\n",
+        "    K=0\n",
+        "    while 2>1:\n",
+        "        K==5\n",
+        "        break\n",
+        "    L.append (K)\n",
+        "    return L"
+      ],
+      "metadata": {
+        "id": "Pf1TgytU9JJJ"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
