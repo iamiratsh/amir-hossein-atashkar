fc5de84db7bfccaa451c2a6c6ae6e753cd0c5c96
iamiratsh <167172029+iamiratsh@users.noreply.github.com>
2024-07-11 16:53:28 +0330

    Created using Colab

---
+++ تابعی بنویسید که 3 عدد صحیح را از ورودی گرفته آنگاه بزرگترین آن را به خروجی ارسال کند.py
@@ -0,0 +1,48 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyPLRM93Z7Oh4COMBTxuL3d7",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/iamiratsh/amir-hossein-atashkar/blob/main/%D8%AA%D8%A7%D8%A8%D8%B9%DB%8C%20%D8%A8%D9%86%D9%88%DB%8C%D8%B3%DB%8C%D8%AF%20%DA%A9%D9%87%203%20%D8%B9%D8%AF%D8%AF%20%D8%B5%D8%AD%DB%8C%D8%AD%20%D8%B1%D8%A7%20%D8%A7%D8%B2%20%D9%88%D8%B1%D9%88%D8%AF%DB%8C%20%DA%AF%D8%B1%D9%81%D8%AA%D9%87%20%D8%A2%D9%86%DA%AF%D8%A7%D9%87%20%D8%A8%D8%B2%D8%B1%DA%AF%D8%AA%D8%B1%DB%8C%D9%86%20%D8%A2%D9%86%20%D8%B1%D8%A7%20%D8%A8%D9%87%20%D8%AE%D8%B1%D9%88%D8%AC%DB%8C%20%D8%A7%D8%B1%D8%B3%D8%A7%D9%84%20%DA%A9%D9%86%D8%AF.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "#تابع بنویسید که 3 عدد صحیح را از ورودی گرفته آنگاه بزرگترین آن را به خروجی ارسال کند :\n",
+        "def max3 (a,b,c) :\n",
+        "    max=a\n",
+        "    if b>max:\n",
+        "       max=b\n",
+        "    if c>max:\n",
+        "       max=c\n",
+        "    return max"
+      ],
+      "metadata": {
+        "id": "Egy2rqst9ibw"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
