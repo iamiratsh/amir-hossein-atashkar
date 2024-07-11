73f2ad58059c67c57ea33cd5d8477f040245e07f
iamiratsh <167172029+iamiratsh@users.noreply.github.com>
2024-07-11 14:09:53 +0330

    Created using Colab

---
+++ METHOD WHITH IS.py
@@ -0,0 +1,100 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyNCr5GWBoy8Odh7D5sjbmjt",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/iamiratsh/amir-hossein-atashkar/blob/main/METHOD%20WHITH%20IS.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "#METOD WHITH IS\n",
+        "# Rozhan Ganavati\n",
+        "\n",
+        "#isalpha #چک میکند که تمام کاراکترهای رشته حروف باشند\n",
+        "#txt = \"HELLO\"\n",
+        "#txt.isalpha()\n",
+        "# True\n",
+        "\n",
+        "#--------------------------------------------------------------------------\n",
+        "\n",
+        "#isalnum #چک میکند که تمام کاراکترهای رشته ترکیب عدد و حروف باشند\n",
+        "#txt = \"hello44\"\n",
+        "#txt.isalnum()\n",
+        "# True\n",
+        "\n",
+        "#txt = \"hello 44\"\n",
+        "#txt.isalnum()\n",
+        "# False\n",
+        "\n",
+        "#---------------------------------------------------------------------------\n",
+        "\n",
+        "#isdigit #چک میکند که تمام کاراکترهای رشته عدد باشند\n",
+        "#a = \"50800\"\n",
+        "#b = \"50800a\"\n",
+        "#a.isdigit() #True\n",
+        "#b.isdigit() #False\n",
+        "\n",
+        "#-----------------------------------------------------------------------------\n",
+        "\n",
+        "#islower #چک میکند که تمام حروف رشته کوچک باشند\n",
+        "#a = \"Hello world\"\n",
+        "#b = \"hello 123\"\n",
+        "\n",
+        "#a.islower() # False\n",
+        "#b.islower() # True\n",
+        "\n",
+        "#------------------------------------------------------------------------------\n",
+        "\n",
+        "#isprintable #چک میکند که تمام حروف رشته قایل پرینت باشند\n",
+        "#txt = \"Hello\\nAre you #1?\"\n",
+        "#txt.isprintable()\n",
+        "# False\n",
+        "\n",
+        "#------------------------------------------------------------------------------\n",
+        "\n",
+        "#isspace #چک میکند که تمام کاراکترهای رشته فاصله باشند\n",
+        "#a = \"   s   \"\n",
+        "#b = \"       \"\n",
+        "\n",
+        "#a.isspace() # False\n",
+        "#b.isspace() # True\n",
+        "\n",
+        "#--------------------------------------------------------------------------------\n",
+        "\n",
+        "#isupper #چک میکند که تمام حروف رشته بزرگ باشند\n",
+        "#txt = \"HELLO\"\n",
+        "#txt.isupper()\n",
+        "# True\n",
+        "\n",
+        "#---------------------------------------------------------------------------------"
+      ],
+      "metadata": {
+        "id": "q1hESz88YC8i"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
