6d4d3ed87304d500a4426e98e3b79ea543ea4f31
iamiratsh <167172029+iamiratsh@users.noreply.github.com>
2024-07-11 16:30:33 +0330

    Created using Colab

---
+++ تابع بنویسید که از ورودی خود لیستی را از اعداد گرفته آنگاه بزرگترین و کوچیکترین به همراه تعداد تکرارشان ب خروجی خود ارسال کند.py
@@ -0,0 +1,46 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyNwSutbKD5biab/LgPi134G",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/iamiratsh/amir-hossein-atashkar/blob/main/%D8%AA%D8%A7%D8%A8%D8%B9%20%D8%A8%D9%86%D9%88%DB%8C%D8%B3%DB%8C%D8%AF%20%DA%A9%D9%87%20%D8%A7%D8%B2%20%D9%88%D8%B1%D9%88%D8%AF%DB%8C%20%D8%AE%D9%88%D8%AF%20%D9%84%DB%8C%D8%B3%D8%AA%DB%8C%20%D8%B1%D8%A7%20%D8%A7%D8%B2%20%D8%A7%D8%B9%D8%AF%D8%A7%D8%AF%20%DA%AF%D8%B1%D9%81%D8%AA%D9%87%20%D8%A2%D9%86%DA%AF%D8%A7%D9%87%20%D8%A8%D8%B2%D8%B1%DA%AF%D8%AA%D8%B1%DB%8C%D9%86%20%D9%88%20%DA%A9%D9%88%DA%86%DB%8C%DA%A9%D8%AA%D8%B1%DB%8C%D9%86%20%D8%A8%D9%87%20%D9%87%D9%85%D8%B1%D8%A7%D9%87%20%D8%AA%D8%B9%D8%AF%D8%A7%D8%AF%20%D8%AA%DA%A9%D8%B1%D8%A7%D8%B1%D8%B4%D8%A7%D9%86%20%D8%A8%20%D8%AE%D8%B1%D9%88%D8%AC%DB%8C%20%D8%AE%D9%88%D8%AF%20%D8%A7%D8%B1%D8%B3%D8%A7%D9%84%20%DA%A9%D9%86%D8%AF.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "#تابع بنویسید که از ورودی خود لیستی را از اعداد گرفته آنگاه بزرگترین و کوچیکترین به همراه تعداد تکرارشان ب خروجی خود ارسال کند\n",
+        "def count(L):\n",
+        "   L.sirt()\n",
+        "   Cout_min=L.count (L[0])\n",
+        "   Cout_man=L.count (L[-1])\n",
+        "   return L [-1] , L[0], count"
+      ],
+      "metadata": {
+        "id": "RN0no_fx4PYf"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
