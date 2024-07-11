6a7199ee425d747fa427924baef417f52869b5ab
iamiratsh <167172029+iamiratsh@users.noreply.github.com>
2024-07-11 13:33:10 +0330

    Created using Colab

---
+++ #برنامه ای بنویسید 3 عدد صحیح a b C را گ.cpp
@@ -0,0 +1,53 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyNuk+cCiZEIOd/9JGpSj7Et",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/iamiratsh/amir-hossein-atashkar/blob/main/%23%D8%A8%D8%B1%D9%86%D8%A7%D9%85%D9%87%20%D8%A7%DB%8C%20%D8%A8%D9%86%D9%88%DB%8C%D8%B3%DB%8C%D8%AF%203%20%D8%B9%D8%AF%D8%AF%20%D8%B5%D8%AD%DB%8C%D8%AD%20a%20b%20C%20%D8%B1%D8%A7%20%DA%AF.cpp\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "#برنامه ای بنویسید 3 عدد صحیح a b C را گرفته آنگاه بزرگترین آن ها را خروجی دهد\n",
+        "#include <iostream>\n",
+        "Main()\n",
+        "{\n",
+        "int a,b,c,max;\n",
+        "Cin >>a>>b>>c;\n",
+        "max=a;\n",
+        "if(b>max)\n",
+        "   max = b;\n",
+        "if(c>max)\n",
+        "   max = c;\n",
+        "Cout<<max;\n",
+        "}"
+      ],
+      "metadata": {
+        "id": "kEqnRG2_PrLJ"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
