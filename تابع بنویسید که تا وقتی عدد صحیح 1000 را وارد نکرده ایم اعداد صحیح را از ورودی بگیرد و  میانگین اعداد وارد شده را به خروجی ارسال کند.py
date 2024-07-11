b301b98c4ddc5f8c8793f52bfee27e68d7f72125
iamiratsh <167172029+iamiratsh@users.noreply.github.com>
2024-07-11 16:32:48 +0330

    Created using Colab

---
+++ تابع بنویسید که تا وقتی عدد صحیح 1000 را وارد نکرده ایم اعداد صحیح را از ورودی بگیرد و  میانگین اعداد وارد شده را به خروجی ارسال کند.py
@@ -0,0 +1,50 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyM7LGWb+Eehj0SC8EpB6qqK",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/iamiratsh/amir-hossein-atashkar/blob/main/%D8%AA%D8%A7%D8%A8%D8%B9%20%D8%A8%D9%86%D9%88%DB%8C%D8%B3%DB%8C%D8%AF%20%DA%A9%D9%87%20%D8%AA%D8%A7%20%D9%88%D9%82%D8%AA%DB%8C%20%D8%B9%D8%AF%D8%AF%20%D8%B5%D8%AD%DB%8C%D8%AD%201000%20%D8%B1%D8%A7%20%D9%88%D8%A7%D8%B1%D8%AF%20%D9%86%DA%A9%D8%B1%D8%AF%D9%87%20%D8%A7%DB%8C%D9%85%20%D8%A7%D8%B9%D8%AF%D8%A7%D8%AF%20%D8%B5%D8%AD%DB%8C%D8%AD%20%D8%B1%D8%A7%20%D8%A7%D8%B2%20%D9%88%D8%B1%D9%88%D8%AF%DB%8C%20%D8%A8%DA%AF%DB%8C%D8%B1%D8%AF%20%D9%88%20%20%D9%85%DB%8C%D8%A7%D9%86%DA%AF%DB%8C%D9%86%20%D8%A7%D8%B9%D8%AF%D8%A7%D8%AF%20%D9%88%D8%A7%D8%B1%D8%AF%20%D8%B4%D8%AF%D9%87%20%D8%B1%D8%A7%20%D8%A8%D9%87%20%D8%AE%D8%B1%D9%88%D8%AC%DB%8C%20%D8%A7%D8%B1%D8%B3%D8%A7%D9%84%20%DA%A9%D9%86%D8%AF.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "#تابع بنویسید که تا وقتی عدد صحیح 1000 را وارد نکرده ایم اعداد صحیح را از ورودی بگیرد و  میانگین اعداد وارد شده را به خروجی ارسال کند\n",
+        "def mean():\n",
+        "    i=0\n",
+        "    Sum=0\n",
+        "    Counter=0\n",
+        "    while i!= 1000:\n",
+        "        i=int(input())\n",
+        "    Sum+=1\n",
+        "    Counter+=1\n",
+        "    return(sum-1000) | ( Counter-1)"
+      ],
+      "metadata": {
+        "id": "Io7Wy21g4zaE"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
