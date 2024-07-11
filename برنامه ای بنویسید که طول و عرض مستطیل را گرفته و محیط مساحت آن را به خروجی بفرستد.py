c6215247e8d2a9fa747e6ae8532a08fad411f46b
iamiratsh <167172029+iamiratsh@users.noreply.github.com>
2024-07-11 16:19:41 +0330

    Created using Colab

---
+++ برنامه ای بنویسید که طول و عرض مستطیل را گرفته و محیط مساحت آن را به خروجی بفرستد.py
@@ -0,0 +1,47 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyPw9cdcHJf222Iui2XZu7ct",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/iamiratsh/amir-hossein-atashkar/blob/main/%D8%A8%D8%B1%D9%86%D8%A7%D9%85%D9%87%20%D8%A7%DB%8C%20%D8%A8%D9%86%D9%88%DB%8C%D8%B3%DB%8C%D8%AF%20%DA%A9%D9%87%20%D8%B7%D9%88%D9%84%20%D9%88%20%D8%B9%D8%B1%D8%B6%20%D9%85%D8%B3%D8%AA%D8%B7%DB%8C%D9%84%20%D8%B1%D8%A7%20%DA%AF%D8%B1%D9%81%D8%AA%D9%87%20%D9%88%20%D9%85%D8%AD%DB%8C%D8%B7%20%D9%85%D8%B3%D8%A7%D8%AD%D8%AA%20%D8%A2%D9%86%20%D8%B1%D8%A7%20%D8%A8%D9%87%20%D8%AE%D8%B1%D9%88%D8%AC%DB%8C%20%D8%A8%D9%81%D8%B1%D8%B3%D8%AA%D8%AF.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "#برنامه ای بنویسید که طول و عرض مستطیل را گرفته و محیط مساحت آن را به خروجی بفرستد\n",
+        "def rect (a,b):\n",
+        "    P=2*(a+b)\n",
+        "    S=a*b\n",
+        "    Print(P , S)\n",
+        "    return P,S\n",
+        ""
+      ],
+      "metadata": {
+        "id": "aQz1HOkz1xvT"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
